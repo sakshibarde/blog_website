b0d15e8b0dfc468d1442f2301643381b9bcc8890
Sakshi Barde <153171863+sakshibarde@users.noreply.github.com>
2024-06-30 14:35:46 +0530

    Add files via upload

---
+++ Blog_page/src/App.jsx
@@ -0,0 +1,16 @@
+import { Navbar,Blogs,Footer} from './components'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <div>
+      <Navbar />
+      <Blogs />
+      <Footer />
+    </div>
+  )
+}
+
+export default App
---
+++ Blog_page/src/components/index.js
@@ -0,0 +1,9 @@
+import Navbar from "./Navbar";
+import Blogs from "./Blogs";
+import Footer from "./Footer";
+
+export {
+    Navbar,
+    Blogs,
+    Footer
+}
