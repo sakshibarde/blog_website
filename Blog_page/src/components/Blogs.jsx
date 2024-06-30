bf015b5761f53c08e3b6d4ed744447fc2065bbee
Sakshi Barde <153171863+sakshibarde@users.noreply.github.com>
2024-06-30 14:41:41 +0530

    Update Blogs.jsx

--- Blog_page/src/components/Blogs.jsx
+++ Blog_page/src/components/Blogs.jsx
@@ -4,7 +4,7 @@ import cover from '../assets/cover.jpeg';
 
 const Blogs = () => {
   return (
-    <div className='w-full bg-[#f9de8a] py-[50px]'>
+    <div className='w-full bg-[#f9f9f9] py-[50px]'>
     <div className='max-w-[1240px] mx-auto'>
         <div className='grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-2 ss:grid-cols-1 gap-8 px-4 text-black'>
             
